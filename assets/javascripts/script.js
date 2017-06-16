957b8d90cd17ab11036f4f14ab7da416de545591
Amy Benson <avbenson@gmail.com>
2017-06-16 12:08:49 +0100

    REG-53 mobile and accessibility fixes

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -4,10 +4,31 @@
 */
 (function ($) {
 
+	/* Expand / collapse filters on small screens */
+	function mobileFilters () {
+		$( "#sidebar-first .block-facetapi" ).wrapAll( "<div class='facets-container mobile-hidden'></div>" );
+		$('.block-current-search').append('<button id="showMobileFilters" type="button" class="show-facets-btn">Show filters</button>');
+		$('#showMobileFilters').click(function () {
+			$('.facets-container').toggleClass('mobile-hidden');
+			$(this).toggleClass('active');
+		});
+	}
+
+	/* Add buttons to filter headings so are focusable */
+	function formatFilters () {
+		$("#sidebar-first .block.block-facetapi h2").each(function () {
+			var label = $(this).text();
+			$(this).html('<button type="button">' + label + '</button>');
+		});
+	}
+
 	$(document).ready(function() {
 
 		//trigger resize
 		$(window).resize();
+
+		mobileFilters();
+		formatFilters();
 
 		//facets
 		$("#sidebar-first .block.block-facetapi").on("click", "h2", function() {
